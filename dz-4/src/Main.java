ee0ee85359562cedbdeba3a7fd804284696d2b16
Valentyna Kovalenko <valentyna.kovalenko@lottoland.com>
2023-04-01 23:35:12 +0200

    My second commit

---
+++ dz-4/src/Main.java
@@ -0,0 +1,10 @@
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Second instSecond = new Second();
+        System.out.printf("~~~%d~~~", instSecond.printMethod());
+
+    }
+}
